4b1f7afccd859cbcbf39cb652818c7338952ad89
Temon137 <ak973@yandex.ru>
2019-03-27 22:49:46 +0400

    Рефакторинг формы подключения к игре.

--- src/trio/view/ConnectionFormController.java
+++ src/trio/view/ConnectionFormController.java
@@ -3,25 +3,20 @@ package trio.view;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import trio.core.Response;
 import trio.core.TrioFacade;
 import trio.game.GameCredentials;
 import trio.game.GameOrchestrator;
+import trio.game.step.AIStepPerformer;
+import trio.game.step.StepPerformer;
 import trio.model.game.Game;
 import trio.model.gamer.Gamer;
 import trio.view.fxml.FXMLManager;
-import trio.view.fxml.RootWithController;
-import trio.game.step.AIStepPerformer;
-import trio.game.step.StepPerformer;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.rmi.RemoteException;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class ConnectionFormController {
@@ -42,141 +37,95 @@ public class ConnectionFormController {
 	}
 	
 	public void init() {
-		createButton.setOnAction(event -> {
-			String gameId = createGame();
-			connectToGame(gameId);
+		createButton.setOnAction(event -> connectToGame(this::createGame));
+		connectButton.setOnAction(event -> connectToGame(this::askGameId));
+	}
+	
+	private void connectToGame(final GameIdSupplier supplier) {
+		wrapTry(() -> {
+			validateGamerName(nameTextField.getText());
+			final String gameId = supplier.getGameId();
+			connectToGame(gameId, new String(nameTextField.getText().getBytes(), StandardCharsets.UTF_8));
 		});
-		connectButton.setOnAction(event -> {
-			String gameId = askGameId();
-			connectToGame(gameId);
-		});
 	}
 	
-	private String createGame() {
+	private void wrapTry(Action action) {
 		try {
-			return getDataFromResponse(trioFacade.createGame());
-		} catch (RemoteException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	private String askGameId() {
-		TextInputDialog dialog = new TextInputDialog();
-		dialog.setHeaderText("Введите ID игры для подключения");
-		dialog.showAndWait();
-		
-		String gameId = dialog.getResult();
-		if (gameId != null && gameId.isEmpty()) gameId = null;
-		return gameId;
-	}
-	
-	private void connectToGame(final String gameId) {
-		try {
-			if (gameId == null || gameId.isEmpty()) return;
-			if (nameTextField.getText() == null || nameTextField.getText().isEmpty()) return;
-			final String gamerName = new String(nameTextField.getText().getBytes(), StandardCharsets.UTF_8);
-			final String gamerId   = getDataFromResponse(trioFacade.connectToGame(gameId, gamerName));
-			if (gamerId == null) return;
-			
-			final Stage stage = showWaiter(gameId);
-			
-			new Thread(() -> {
-				try {
-					while (true) {
-						Game game = getDataFromResponse(trioFacade.getGameState(gameId, gamerId));
-						if (game == null) {
-							Platform.runLater(() -> end(null, null));
-							return;
-						}
-						if (game.getGamers().size() == 2) {
-							Platform.runLater(() -> end(gameId, gamerId));
-							return;
-						}
-						
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-				} catch (RemoteException e) {
-					e.printStackTrace();
-					showError(e.getMessage());
-					Platform.runLater(() -> end(null, null));
-				} finally {
-					Platform.runLater(stage::close);
-				}
-			}).start();
-		} catch (RemoteException e) {
-			e.printStackTrace();
+			action.start();
+		} catch (Exception e) {
 			showError(e.getMessage());
+			e.printStackTrace();
 		}
+	}
+	
+	private void validateGamerName(String gamerName) throws Exception {
+		if (gamerName == null || gamerName.isEmpty()) {
+			throw new Exception("Имя игрока не может быть пустым.");
+		}
+	}
+	
+	private void connectToGame(String gameId, String gamerName) throws Exception {
+		final String gamerId = getDataFromResponse(trioFacade.connectToGame(gameId, gamerName));
+		
+		final Stage stage = showWaiter(gameId);
+		
+		new Thread(() -> {
+			wrapTry(() -> {
+				while (true) {
+					Game game = getDataFromResponse(trioFacade.getGameState(gameId, gamerId));
+					if (game.getGamers().size() == 2) {
+						Platform.runLater(() -> startGame(new GameCredentials(gameId, gamerId, gamerName)));
+						return;
+					}
+					
+					Thread.sleep(1000);
+				}
+			});
+			Platform.runLater(stage::close);
+		}).start();
 	}
 	
 	private void showError(String text) {
-		Alert alert = new Alert(Alert.AlertType.ERROR);
-		alert.setTitle("Ошибка");
-		alert.setHeaderText("Произошла ошибка!");
-		alert.setContentText(text);
-		alert.show();
+		Platform.runLater(() -> {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Ошибка");
+			alert.setHeaderText("Произошла ошибка!");
+			alert.setContentText(text);
+			alert.show();
+		});
 	}
 	
-	private <T> T getDataFromResponse(Response<T> response) {
-		T data;
+	private <T> T getDataFromResponse(Response<T> response) throws Exception {
 		if (response.hasError()) {
-			showError(response.getErrorText());
-			data = null;
-		} else {
-			data = response.getData();
+			throw new Exception(response.getErrorText());
 		}
-		return data;
+		return response.getData();
 	}
 	
 	private Stage showWaiter(String gameId) {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("forms/WaiterForm.fxml"));
-		Parent     root   = null;
-		try {
-			root = loader.load();
-		} catch (IOException e) {
-			System.err.println("Если это произошло, то всё пропало.");
-		}
-		WaiterController controller = loader.getController();
-		controller.init(gameId);
-		Stage stage = new Stage();
-		stage.setResizable(false);
-//		stage.initStyle(StageStyle.UNDECORATED);
-		Scene scene = new Scene(root);
-		stage.setScene(scene);
+		final var waiterForm = FXMLManager.<WaiterController>load("WaiterForm");
+		waiterForm.getController().init(gameId);
+		final Stage stage = FXMLManager.createStage(waiterForm.getRoot(), "Ожидайте");
 		stage.show();
 		return stage;
 	}
 	
-	private void end(String gameId, String gamerId) {
-		System.out.println(gameId + " " + gamerId);
+	private void startGame(GameCredentials creds) {
+		AtomicReference<Game> gameRef = new AtomicReference<>();
+		wrapTry(() -> gameRef.set(getDataFromResponse(trioFacade.getGameState(creds.getGameId(), creds.getGamerId()))));
+		Game game = gameRef.get();
 		
-		Game game;
-		try {
-			game = trioFacade.getGameState(gameId, gamerId).getData();
-		} catch (RemoteException e) {
-			showError(e.getMessage());
-			return;
-		}
+		final String opponentGamerName = game.getGamers().parallelStream()
+		                                     .map(Gamer::getName)
+		                                     .filter(s -> !s.equals(creds.getGamerName()))
+		                                     .findAny().orElseThrow();
 		
-		Gamer primoGamer = game.getGamers().get(0).getName().equals(nameTextField.getText())
-		                   ? game.getGamers().get(0)
-		                   : game.getGamers().get(1);
-		Gamer secundoGamer = !game.getGamers().get(0).getName().equals(nameTextField.getText())
-		                     ? game.getGamers().get(0)
-		                     : game.getGamers().get(1);
-		
-		
-		RootWithController<GameFormController> gameForm = FXMLManager.load("GameForm");
+		final var gameForm = FXMLManager.<GameFormController>load("GameForm");
 		gameForm.getController().init(game.getField().getWidth(),
 		                              game.getField().getHeight(),
-		                              primoGamer.getName(),
-		                              secundoGamer.getName());
-		Stage stage = FXMLManager.createStage(gameForm.getRoot(), "Игра");
+		                              creds.getGamerName(),
+		                              opponentGamerName);
+		Stage stage = FXMLManager.createStage(gameForm.getRoot(), "Trio");
 		stage.setOnCloseRequest(event -> System.exit(1));
 		stage.show();
 		
@@ -188,20 +137,39 @@ public class ConnectionFormController {
 		}
 		
 		GameOrchestrator gameOrchestrator = new GameOrchestrator(trioFacade,
-		                                                         new GameCredentials(gameId,
-		                                                                             gamerId,
-		                                                                             nameTextField.getText()),
+		                                                         creds,
 		                                                         gameForm.getController(),
 		                                                         stepPerformer);
-		Thread thread = new Thread(() -> {
-			try {
-				gameOrchestrator.start();
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
-		});
+		Thread thread = new Thread(() -> wrapTry(gameOrchestrator::start));
 		thread.start();
 		
 		nameTextField.getScene().getWindow().hide();
+	}
+	
+	private String createGame() throws Exception {
+		return getDataFromResponse(trioFacade.createGame());
+	}
+	
+	private String askGameId() throws Exception {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setHeaderText("Введите ID игры для подключения");
+		dialog.showAndWait();
+		
+		String gameId = dialog.getResult();
+		if (gameId == null || gameId.isEmpty()) {
+			throw new Exception("Пустой ID. Подключение невозможно.");
+		}
+		return gameId;
+	}
+	
+	@FunctionalInterface
+	private interface GameIdSupplier {
+		String getGameId() throws Exception;
+	}
+	
+	
+	@FunctionalInterface
+	private interface Action {
+		void start() throws Exception;
 	}
 }
